00e4b2a6f96b5336c86f4c0cad916c1fb061b1f7
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-18 22:08:52 +0300

    nodemon added

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 router.get("/", (req, res) => {
-    return res.send("Home Page LAST changed")
+    return res.send("Home Page LAST changed123")
 })
 
 module.exports = router
