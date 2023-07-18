2693572e2686a7c0569ccbf095946e5e0b79b907
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-18 22:17:20 +0300

    bootstrap npm

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const app = express()
 
+const path = require("path");
+
 app.set("view engine", "ejs")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 const userRoutes = require("./routes/user")
 
